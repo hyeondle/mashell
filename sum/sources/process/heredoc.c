ae56eb65c376ce00746eb6a88940f06fe0efc417
Hyeon <linsio@Hyeonui-MacBookAir.local>
2023-05-02 09:56:56 +0900

    a

--- sum/sources/process/heredoc.c
+++ sum/sources/process/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: Linsio <Linsio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 10:05:43 by hyeondle          #+#    #+#             */
-/*   Updated: 2023/05/01 23:10:34 by Linsio           ###   ########.fr       */
+/*   Updated: 2023/05/02 09:52:21 by Linsio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,22 +50,25 @@ void	init_t_heredoc(t_heredoc *hdoc)
 	hdoc->size_heredoc = 0;
 }
 
-void hd_handler(int sig, siginfo_t *info, void *oldsiga)
-{
-	if (sig == SIGINT)
-		exit(0);
-}
+// void hd_handler(int sig, siginfo_t *info, void *oldsiga)
+// {
+// 	if (sig == SIGINT)
+// 		/*
+// 		원래 히어독 종료랑 똑같이 작동하도록 구현해야함. 즉, 임시파일 삭제와
+// 		파싱 종료, 부모 프로세스로 돌아갈때까지만 exit, 지켜주도록 해야함. 그러면 끝
+// 		*/
+// }
 
-void	init_hd_sig(void)
-{
-	struct sigaction	act;
+// void	init_hd_sig(void)
+// {
+// 	struct sigaction	act;
 
-	rl_catch_signals = 1;
-	act.sa_flags = SA_SIGINFO;
-	act.sa_sigaction = hd_handler;
-	sigemptyset(&act.sa_mask);
-	sigaction(SIGINT, &act, NULL);
-}
+// 	rl_catch_signals = 1;
+// 	act.sa_flags = SA_SIGINFO;
+// 	act.sa_sigaction = hd_handler;
+// 	sigemptyset(&act.sa_mask);
+// 	sigaction(SIGINT, &act, NULL);
+// }
 
 t_heredoc	*do_heredoc(char **arg, t_deque *deque)
 {
@@ -75,7 +78,7 @@ t_heredoc	*do_heredoc(char **arg, t_deque *deque)
 	init_t_heredoc(hdoc);
 	parsing_heredoc(arg, hdoc);
 
-	init_hd_sig();
+	// init_hd_sig();
 	get_heredoc(hdoc);
 
 	cnt_heredoc_in_node(deque);
--- sum/sources/process/heredoc2.c
+++ sum/sources/process/heredoc2.c
@@ -6,7 +6,7 @@
 /*   By: Linsio <Linsio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 10:05:44 by hyeondle          #+#    #+#             */
-/*   Updated: 2023/05/01 23:15:39 by Linsio           ###   ########.fr       */
+/*   Updated: 2023/05/02 09:56:44 by Linsio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,36 @@ void	distribute_heredoc(t_deque *deque, t_heredoc *hdoc)
 		node = node->next;
 	}
 }
+//여기서부터
+void	sig1(void)
+{
+	exit(0);
+}
+static void	handler(int sig, siginfo_t *info, void *oldsiga)
+{
+	if (sig == SIGQUIT)
+		rl_redisplay();
+	else if (sig == SIGINT)
+	{
+		rl_replace_line("", 1);
+		rl_on_new_line();
+		printf("\n");
+		rl_redisplay();
+	}
+}
 
+void	init_signalaction2(void)
+{
+	struct sigaction	act;
+
+	rl_catch_signals = 0;
+	act.sa_flags = SA_SIGINFO;
+	act.sa_sigaction = handler;
+	sigemptyset(&act.sa_mask);
+	sigaction(SIGINT, &act, NULL);
+	sigaction(SIGQUIT, &act, NULL);
+}
+// 여기까지 다른데로 옮김
 int	ft_here_doc(t_heredoc *hdoc, int idx)
 {
 	char	*str;
@@ -65,6 +94,8 @@ int	ft_here_doc(t_heredoc *hdoc, int idx)
 		exit(1);
 	if (pid == 0)
 	{
+		rl_catch_signals = 1; // 이건 sig1함수안에 넣어도 됨
+		signal(SIGINT, sig1); // 이걸 .here_doc파일 비우는걸로 제작
 		while (1)
 		{
 			str = readline("> ");
@@ -80,5 +111,6 @@ int	ft_here_doc(t_heredoc *hdoc, int idx)
 		}
 	}
 	wait(NULL);
+	init_signalaction2(); // 이건 메인에 있는것과 같음. 그냥 이 함수말고 외부함수에서 한번 더 호출해도 됨
 	return (here_fd);
 }
